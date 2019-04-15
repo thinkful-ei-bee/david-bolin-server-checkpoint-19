979bcdb044cd51769413bc13fc1462fd4fe431e1
David Bolin <david@bolin.at>
2019-04-15 02:01:06 -0700

    preparing deployment

--- src/server.js
+++ src/server.js
@@ -12,5 +12,6 @@ const db = knex({
 app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  // eslint-disable-next-line no-console
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
